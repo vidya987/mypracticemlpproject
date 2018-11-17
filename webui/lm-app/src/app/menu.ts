76bb0632d08ca1df9056e0e83fb3868e3ac81843
PadmanabhanE <PadmanabhanE@hexaware.com>
2018-11-17 19:56:08 +0530

    clean changes

---
+++ webui/lm-app/src/app/menu.ts
@@ -0,0 +1,9 @@
+export class Menu {
+    constructor(public foodId: string,
+        public foodName: string,
+        public quantity: string,
+        public price: string,
+        public vendor: string){
+            
+        }
+}
