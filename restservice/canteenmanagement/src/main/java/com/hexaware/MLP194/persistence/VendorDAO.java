1af88cbf73434b56bdd668ef27898d03124cfb63
Cathu <kirubacatherine6398@gmail.com>
2020-02-03 10:04:23 +0530

    Dothat

---
+++ restservice/canteenmanagement/src/main/java/com/hexaware/MLP194/persistence/VendorDAO.java
@@ -0,0 +1,17 @@
+package com.hexaware.MLP194.persistence;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
+import java.util.List;
+import com.hexaware.MLP194.model.Vendor;
+/**
+ * VendorDAO class used to fetch data from data base.
+ * @author hexware
+ */
+public interface VendorDAO {
+    /**
+     * @return the all the Vendor record.
+     */
+  @SqlQuery("Select * from Vendor")
+    @Mapper(VendorMapper.class)
+    List<Vendor> show();
+}
---
+++ restservice/canteenmanagement/src/main/java/com/hexaware/MLP194/persistence/VendorMapper.java
@@ -0,0 +1,27 @@
+package com.hexaware.MLP194.persistence;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.hexaware.MLP194.model.Vendor;
+
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+import org.skife.jdbi.v2.StatementContext;
+/**
+ * vENDORMapper class used to fetch menu data from database.
+ */
+public class VendorMapper implements ResultSetMapper<Vendor> {
+    /**
+     * @param idx the index
+     * @param rs the resultset
+     * @param ctx the context
+     * @return the mapped Vendor object
+     * @throws SQLException in case there is an error in fetching data from the resultset
+     */
+  public final Vendor map(final int idx, final ResultSet rs, final StatementContext ctx) throws SQLException {
+      /**
+       * @return Vendor
+       */
+    return new Vendor(rs.getInt("vdrId"), rs.getString("status"), rs.getString("spl"));
+  }
+}
