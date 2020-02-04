b35f9fa885cdeb17fcf5d3f20e97dc0608928524
Cathu <kirubacatherine6398@gmail.com>
2020-02-04 15:35:12 +0530

    checking

--- restservice/canteenmanagement/src/main/java/com/hexaware/MLP194/util/CliMain.java
+++ restservice/canteenmanagement/src/main/java/com/hexaware/MLP194/util/CliMain.java
@@ -1,6 +1,8 @@
+
 package com.hexaware.MLP194.util;
 
 import java.util.Scanner;
+<<<<<<< HEAD
 
 import com.hexaware.MLP194.factory.CustomerFactory;
 //import com.hexaware.MLP194.factory.MenuFactory;
@@ -8,6 +10,12 @@ import com.hexaware.MLP194.model.Customer;
 //import com.hexaware.MLP194.persistence.CustomerDAO;
 //import com.hexaware.MLP194.model.Customer;
 //import com.hexaware.MLP194.model.Menu;
+=======
+import com.hexaware.MLP194.factory.MenuFactory;
+import com.hexaware.MLP194.factory.VendorFactory;
+import com.hexaware.MLP194.model.Menu;
+import com.hexaware.MLP194.model.Vendor;
+>>>>>>> 54665875a61859c510b273abdc5b238339b4a5dd
 /**
  * CliMain used as Client interface for java coading.
  * @author hexware
@@ -20,12 +28,21 @@ class CliMain {
   private void mainMenu() {
     System.out.println("Canteen Management System");
     System.out.println("-----------------------");
+<<<<<<< HEAD
     System.out.println("1. Insert into Customer");
     System.out.println("2.  display customer");
     System.out.println("3. Update customer");
     System.out.println("4. Delete a customer");
     System.out.println("5. Exit");
 
+=======
+    System.out.println("1. Show Menu");
+    System.out.println("2.display vendor details");
+    System.out.println("3.add vendor details");
+    System.out.println("4.update vendor details");
+    System.out.println("5.delete vendor details");
+    System.out.println("6. Exit");
+>>>>>>> 54665875a61859c510b273abdc5b238339b4a5dd
     mainMenuDetails();
   }
 /**
@@ -34,8 +51,9 @@ class CliMain {
   private void mainMenuDetails() {
     try {
       System.out.println("Enter your choice:");
-      int menuOption = option.nextInt();
+      final int menuOption = option.nextInt();
       switch (menuOption) {
+<<<<<<< HEAD
         case 1:
           addCustomers();
           break;
@@ -53,14 +71,36 @@ class CliMain {
           break;
         default:
           System.out.println("Choose either 1 or 2 or 3");
+=======
+      case 1:
+        showFullMenu();
+        break;
+        case 2:
+        displayVendor();
+        break;
+        case 3:
+        addVendor();
+        break;
+        case 4:
+        updateVendor();
+        break;
+        case 5:
+        deleteVendor();
+        break;
+      case 6:
+        Runtime.getRuntime().halt(0);
+      default:
+        System.out.println("Choose either 1 or 2");
+>>>>>>> 54665875a61859c510b273abdc5b238339b4a5dd
       }
-    } catch (Exception e) {
+    } catch (final Exception e) {
       e.printStackTrace();
       System.out.println("enter a valid value");
     }
     option.nextLine();
     mainMenu();
   }
+<<<<<<< HEAD
   /**
    * showFullMenu method display the menu item stored in database.
    */
@@ -84,8 +124,65 @@ class CliMain {
       System.out.println("inserted successfully");
     } else {
       System.out.println("Not Inserted");
+=======
+
+  /**
+   * showFullMenu method display the menu item stored in database.
+   */
+  private void showFullMenu() {
+    final Menu[] menu = MenuFactory.showMenu();
+    System.out.println("itmId" + "\t" + "itmName");
+
+    for (final Menu m : menu) {
+      System.out.println(m.getitmId() + "\t" + m.getitmName());
+>>>>>>> 54665875a61859c510b273abdc5b238339b4a5dd
     }
   }
+  private void displayVendor() {
+    Vendor[] me = VendorFactory.showVendor();
+    System.out.println("vdrId" + "\t" + "spl" + "\t" + "status");
+
+    for (Vendor m : me) {
+      System.out.println(m.getVdrId() + "\t" + m.getSpl() + "\t" + "\t" + m.getStatus());
+    }
+  }
+
+
+  private void addVendor() {
+    System.out.println( "spl" + "\t" + "status" + "\t" + "vdrId");
+    final String spl = option.next();
+    final String status = option.next();
+    final int vdrId = option.nextInt();
+    final int i = VendorFactory.insertingVendor(spl, status, vdrId);
+    if(i > 0){
+      System.out.println("inserted successfully");
+    } else {
+      System.out.println("Not Inserted");
+    }
+ }
+ private void updateVendor() {
+  System.out.println("update spl" + "\t" + "vdrId");
+  String spl = option.next();
+    int vdrId = option.nextInt();
+    int i = VendorFactory.updatingVendor(spl, vdrId);
+    if(i >= 0){
+      System.out.println("updated successfully");
+    } else {
+      System.out.println("Not updated");
+    }
+  }
+  private void deleteVendor() {
+    System.out.println("delete vdrId");
+    int vdrId = option.nextInt();
+    int i = VendorFactory.deletingVendor(vdrId);
+    if (i > 0) {
+      System.out.println("deleted successfully");
+    } else {
+      System.out.println("not deleted");
+    }
+  }
+
+
 
   private void updateCustomer() {
     System.out.println("Enter Customer number");
